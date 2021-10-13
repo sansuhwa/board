8727b30eeb1f4704bac32b2f322c39ed8a57a429
sansuhwa <everi1007@gmil.com>
2021-10-13 17:38:09 +0900

    add detail

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 
 from boardapp.views import board_list
 
+from boardapp.views import create_post
+
+from boardapp.views import detail_post
 
 urlpatterns = [
     #board/
-    path('', board_list, name='board_list')
+    path('', board_list, name='board_list'),
+    path('create/', create_post, name='creat_post'),
+    #board/<post_id>
+    path('<int:post_id>/', detail_post, name='detail.post')
 ]
--- boardapp/views.py
+++ boardapp/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
-from boardapp.models import text
+from boardapp.models import text, Post
+from django.http.response import HttpResponse
 # Create your views here.
 
 def board_list(request):
-    text_objects = text.objects.all()
+    post_objects = Post.objects.all()
     context = {
-        'posts': text_objects
+        'posts': post_objects
     }
     return render(request, 'board_list.html', context=context)
+
+def detail_post(requset, post_id):
+    detail_post=Post.objects.get(id=post_id)
+    context = {
+        'post': detail_post
+    }
+    return render(requset, 'detail_post.html', context=context)
+    
+
+
+def create_post(request):
+    if request.method == 'GET':
+        return render(request, 'create_post.html')
+    if request.method == 'POST':
+        text_model = request.POST
+        text_model_title = text_model['title']
+        text_model_description = text_model['description']
+        post = text.objects.create(title=f'{text_model_title}', description=f'{text_model_description}')
+        context = {
+           'post': post
+        }
+        return render(request, 'detail_post.html',context=context)
+        
+        
+        
