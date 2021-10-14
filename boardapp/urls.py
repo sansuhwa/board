b761d21d8f6da0606644d8b661de5f1fd45e5472
sansuhwa <everi1007@gmil.com>
2021-10-14 20:41:08 +0900

    add update

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -6,10 +6,14 @@ from boardapp.views import create_post
 
 from boardapp.views import detail_post
 
+from boardapp.views import update_post
+
 urlpatterns = [
     #board/
     path('', board_list, name='board_list'),
     path('create/', create_post, name='creat_post'),
-    #board/<post_id>
-    path('<int:post_id>/', detail_post, name='detail.post')
+    #board/<post_id>/
+    path('<int:post_id>/', detail_post, name='detail_post'),
+    #board/<post_id>/update/
+    path('<int:post_id>/update/', update_post, name='update_post')
 ]
--- boardapp/views.py
+++ boardapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from boardapp.models import text, Post
 from django.http.response import HttpResponse
 # Create your views here.
@@ -18,7 +18,6 @@ def detail_post(requset, post_id):
     return render(requset, 'detail_post.html', context=context)
     
 
-
 def create_post(request):
     if request.method == 'GET':
         return render(request, 'create_post.html')
@@ -26,11 +25,37 @@ def create_post(request):
         text_model = request.POST
         text_model_title = text_model['title']
         text_model_description = text_model['description']
-        post = text.objects.create(title=f'{text_model_title}', description=f'{text_model_description}')
+        post = Post.objects.create(title=f'{text_model_title}', description=f'{text_model_description}')
         context = {
            'post': post
         }
-        return render(request, 'detail_post.html',context=context)
-        
-        
-        
+        return render(request, 'detail_post.html', context=context)
+
+def update_post(requset, post_id):
+    if requset.method == 'GET':
+        post = Post.objects.get(id=post_id)
+        context = {
+            'post': post
+        }
+        return render(requset, 'update_post.html', context=context)
+    if requset.method == 'POST':
+        post_model: dict = requset.POST
+        post_model_title = post_model['title']
+        post_model_description = post_model['description']
+        post = Post.objects.get(id=post_id)
+        post.title = post_model_title
+        post.description = post_model_description
+        post.save()
+        context = {
+            'post': post
+        }
+        return render(requset, 'detail_post.html', context=context)
+
+
+def delete_post(req, post_id):
+    post=Post.objects.get(id=post_id)
+    post.delete()
+    post.save()
+    response = redirect('/board/')
+    return response
+    
